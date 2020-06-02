767b6685890b7fdc11364bd23dd5356335c97dc4
krishna <36997722+krishnaagarwal1feb@users.noreply.github.com>
2020-06-02 14:08:31 +0530

    Update setup.py

--- DataEngineering/setup.py
+++ DataEngineering/setup.py
@@ -1,9 +1,9 @@
 import mysql.connector as mysql
 db = mysql.connect(user='root', password='',host='localhost')
 cursor = db.cursor()
-cursor.execute("create database db1;")
-cursor.execute("use db1;")
-createDB = "create table data(  id varchar(255),  commonName varchar(255), lat float, lon float, time TIME, NbBikes varchar(20),primary key(id,time));"
+cursor.execute("create database db;")
+cursor.execute("use db;")
+createDB = "create table data(  id varchar(255),  commonName varchar(255), lat float, lon float, time TIME, NbBikes int,NbDocks int,primary key(id,time));"
 cursor.execute(createDB)
 db.commit()
 cursor.close()
