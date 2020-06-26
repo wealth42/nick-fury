a01fc8496bb37ba6383d7f0754683c5a73abe408
pradnyalgandhi <pradnyalgandhi28@gmail.com>
2020-06-26 16:43:37 +0530

    Settting up the Database

---
+++ setup.py
@@ -0,0 +1,29 @@
+# Setting up the Local Database with MySQL
+import mysql.connector
+
+db = mysql.connector.connect(user="admin", #Your Database Username
+							  password = "admin", # Your Database Password
+							  host = "localhost",
+							  )
+
+# Making a cursor object for Database
+my_cursor = db.cursor()
+
+# Creating the Database
+my_cursor.execute("CREATE DATABASE local_database")
+
+""" Creating Table "info" with following Columns
+	*id - Primary Key
+	*commonName - Name of the Place	
+	*lat - Latitude of the Place
+	*lon - Longitude of the Place
+	*nbBikes - Number of Bikes
+	*nbDocks - Number of Docks
+	*nbEmpty - Number of Empty Docks
+	*time - Current Time
+"""
+my_cursor.execute("USE local_database")
+my_cursor.execute("CREATE TABLE info(id VARCHAR(255), commonName VARCHAR(255),  lat float, lon float, nbBikes VARCHAR(10), nbDocks VARCHAR(10),nbEmpty VARCHAR(10),time VARCHAR(255))")
+db.commit()
+db.close()
+
