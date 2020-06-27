bdd594b26b239b663e6aacba3c17cb59ca144814
Ankit chaudhary <40197389+ankitcdry1612@users.noreply.github.com>
2020-06-27 12:38:54 +0530

    Add files via upload

--- nick-fury/create_database.py
+++ nick-fury/create_database.py
@@ -10,7 +10,7 @@ con = MySQLdb.connect(host=secret.host,
 cur = con.cursor()
 
 try:
-	cur.execute("CREATE DATABASE secret.db2;")
+	cur.execute("CREATE DATABASE "+secret.db1+";")
 
 	db = MySQLdb.connect(host=secret.host,
 						user=secret.user,
@@ -21,20 +21,21 @@ try:
 	cursor.execute("CREATE TABLE terminal(Terminal_id VARCHAR(45) NOT NULL, CommonName VARCHAR(200) NOT NULL, TerminalName VARCHAR(45) NOT NULL, PRIMARY KEY (Terminal_id));")
 	db.commit()
 
-	cursor.execute("CREATE TABLE terminal_info(TerminalName VARCHAR(45) NOT NULL, nbBike int(11) NOT NULL, nbEmptyDock int(11) NOT NULL, nbDock int(11) NOT NULL, datet datetime NOT NULL, PRIMARY KEY (TerminalName,datet));")
+	cursor.execute("CREATE TABLE terminal_info(TerminalName VARCHAR(45) NOT NULL, nbBike int(11) NOT NULL, nbEmptyDock int(11) NOT NULL, nbDock int(11) NOT NULL, datet datetime NOT NULL, PRIMARY KEY (TerminalName));")
 	db.commit()
 
 except Exception as e:
-	print("Database "+ secret.db2 +" already exist")
+	print("Database "+ secret.db1 +" already exist")
 	pass
 
 try:
-	cur.execute("CREATE DATABASE secret.db1;")
+	db2=secret.db2
+	cur.execute("CREATE DATABASE "+secret.db2+";")
 
 	db = MySQLdb.connect(host=secret.host,
 						user=secret.user,
 						passwd=secret.password,
-						db=secret.db1)
+						db=secret.db2)
 
 	cursor = db.cursor()
 	cursor.execute("CREATE TABLE terminal(Terminal_id VARCHAR(45) NOT NULL, CommonName VARCHAR(200) NOT NULL, TerminalName VARCHAR(45) NOT NULL, PRIMARY KEY (Terminal_id));")
@@ -42,6 +43,6 @@ try:
 
 	cursor.execute("CREATE TABLE terminal_info(TerminalName VARCHAR(45) NOT NULL, nbBike INT(11) NOT NULL, nbEmptyDock INT(11) NOT NULL, nbDock INT(11) NOT NULL, datet datetime NOT NULL, PRIMARY KEY (TerminalName,datet));")
 	db.commit()
-except:
-	print("Database "+ secret.db1 +" already exist")
+except Exception as e:
+	print("Database "+ secret.db2 +" already exist")
 	pass
