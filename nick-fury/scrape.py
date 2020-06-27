ee5e71d8f046096030ade205d01b4c8e65512b5c
Ankit chaudhary <40197389+ankitcdry1612@users.noreply.github.com>
2020-06-27 12:39:25 +0530

    Add files via upload

--- nick-fury/scrape.py
+++ nick-fury/scrape.py
@@ -34,7 +34,11 @@ def task(con1,cur1,con2,cur2):
 				elif(j['key']=='NbDocks'):
 					info['nbDock']=j['value']
 
-			d = data[i]['additionalProperties'][6]['modified'].split('T')
+			for j in range(len(data[i]['additionalProperties'])):
+				if("modified" in data[i]['additionalProperties'][j]):
+					d=data[i]['additionalProperties'][j]['modified'].split('T')
+					break
+
 			date = list(map(int,d[0].split("-")))
 			Time = d[1].split('.')[0].split(':')
 			if('Z' in Time[2]):
@@ -44,33 +48,28 @@ def task(con1,cur1,con2,cur2):
 			dateTime = datetime.datetime(date[0],date[1],date[2],Time[0],Time[1],Time[2])
 			info['datetime']=dateTime
 
-			query = """SELECT * FROM terminal_info WHERE TerminalName=%s AND datet=%s;"""
-			cur1.execute(query,(info['terminalName'],info['datetime'],))
-			result = cur1.fetchall()
-			if(len(result)==0):
-				query = """DELETE FROM terminal_info WHERE TerminalName=%s;"""
-				cur1.execute(query,(info['terminalName'],))
-				con1.commit()
 
-				query = """INSERT INTO terminal_info(TerminalName,nbBike,nbEmptyDock,nbDock,datet) VALUES(%s,%s,%s,%s,%s);"""
-				tup = (info['terminalName'],info['nbBike'],info['nbEmptyDock'],info['nbDock'],info['datetime'])
-				cur1.execute(query,tup)
-				con1.commit()
+			query = """INSERT INTO terminal_info(TerminalName,nbBike,nbEmptyDock,nbDock,datet) VALUES(%s,%s,%s,%s,%s) ON DUPLICATE KEY UPDATE \
+					TerminalName=VALUES(TerminalName),\
+					nbBike=VALUES(nbBike),\
+					nbEmptyDock=VALUES(nbEmptyDock),\
+					nbDock=VALUES(nbDock),\
+					datet=VALUES(datet);"""
 
-				cur2.execute(query,tup)
-				con2.commit()
+			tup = (info['terminalName'],info['nbBike'],info['nbEmptyDock'],info['nbDock'],info['datetime'])
+			cur1.execute(query,tup)
+			con1.commit()
+
+			query = """INSERT IGNORE INTO terminal_info(TerminalName,nbBike,nbEmptyDock,nbDock,datet) VALUES(%s,%s,%s,%s,%s);"""
+			cur2.execute(query,tup)
+			con2.commit()
 		
-		
-			query = """SELECT * FROM terminal WHERE Terminal_id=%s;"""
-			cur1.execute(query,(terminal['id'],))
-			result = cur1.fetchall()
-			if(len(result)==0):
-				query = """INSERT INTO terminal(Terminal_id,CommonName,TerminalName) VALUES(%s,%s,%s);"""
-				cur1.execute(query,(terminal['id'],terminal['commonName'],terminal['terminalName'],))
-				con1.commit()
+			query = """INSERT IGNORE INTO terminal(Terminal_id,CommonName,TerminalName) VALUES(%s,%s,%s);"""
+			cur1.execute(query,(terminal['id'],terminal['commonName'],terminal['terminalName'],))
+			con1.commit()
 
-				cur2.execute(query,(terminal['id'],terminal['commonName'],terminal['terminalName'],))
-				con2.commit()			
+			cur2.execute(query,(terminal['id'],terminal['commonName'],terminal['terminalName'],))
+			con2.commit()			
 		except Exception as e:
 			print(e)
 			break
@@ -78,4 +77,9 @@ def task(con1,cur1,con2,cur2):
 	print("Excution time :",time.time()-start)
 	print("DONE")
 	print()
-	
+
+url = "https://api.tfl.gov.uk/bikepoint"
+response = requests.get(url)
+data = response.text
+data = json.loads(data)
+
