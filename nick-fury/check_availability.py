e411cf01d25a1a0549305d1a821ad6a154548347
Ankit chaudhary <40197389+ankitcdry1612@users.noreply.github.com>
2020-06-27 11:04:10 +0530

    Add files via upload

--- nick-fury/check_availability.py
+++ nick-fury/check_availability.py
@@ -1,27 +1,36 @@
 import sys
 import MySQLdb
+import secret
 
+#This file supposed to be run separately
 
-con1 = MySQLdb.connect(host='localhost',
-							user='root',
-							passwd="ankit@02217",
-							db='realtimebikepoint')
-cur1 = con1.cursor()
-
+def connection():
+	con = MySQLdb.connect(host=secret.host,
+							user=secret.user,
+							passwd=secret.password,
+							db=secret.db1)
+	cur = con.cursor()
+	return con,cur
 
 def strip(s):
 	return s.strip().lower()
 
-argument = " ".join(sys.argv[1:])
-commonName = " ".join(list(map(strip,argument.split(','))))
+def check(commonName,cur):
+	query = """SELECT terminal_info.TerminalName,terminal_info.nbBike,terminal_info.nbEmptyDock,terminal_info.nbDock,terminal_info.datet FROM terminal_info,terminal WHERE CommonName=%s and terminal.TerminalName=terminal_info.TerminalName;"""
+	cur.execute(query,(commonName,))
+	ans = cur.fetchall()[0]
+
+	print("Terminal name :",ans[0])
+	print("No. of bikes available :",ans[1])
+	print("No. of empty docs :",ans[2])
+	print("No. of total docks :",ans[3])
+	print("Datetime :",ans[4])	
 
 
-query = """SELECT terminal_info.TerminalName,terminal_info.nbBike,terminal_info.nbEmptyDock,terminal_info.nbDock,terminal_info.datet FROM terminal_info,terminal WHERE CommonName=%s and terminal.TerminalName=terminal_info.TerminalName;"""
-cur1.execute(query,(commonName,))
-ans = cur1.fetchall()[0]
+if __name__ == "__main__":	
+	con,cur=connection()
+	argument = " ".join(sys.argv[1:])
+	commonName = " ".join(list(map(strip,argument.split(','))))
+	check(commonName,cur)
 
-print("Terminal name :",ans[0])
-print("No. of bikes available :",ans[1])
-print("No. of empty docs :",ans[2])
-print("No. of total docks :",ans[3])
-print("Datetime :",ans[4])
+
