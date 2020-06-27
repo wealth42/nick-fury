922c8a4838d8d83efad49c115ecaec754e51e36b
Ankit chaudhary <40197389+ankitcdry1612@users.noreply.github.com>
2020-06-27 11:03:02 +0530

    Add files via upload

---
+++ nick-fury/secret.py
@@ -0,0 +1,5 @@
+host = ""
+user = ""
+password = ""
+db1 = "realtimebikepoint"   #database for realtime bike availabilty storage
+db2 = "bikepoint"			#database for bike availability storage in different timestamp
