5627b1995e371ad7ff6f506261a9a84f8acb423b
Hrishikesh Upadhyaya <chinum.upadhyaya@gmail.com>
2021-05-02 22:36:00 +0530

    Login Component Completed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Fragment} from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './components/auth/Login'
+
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <Fragment>
+      <Route exact path='/' component={Login} />
+      
+    </Fragment>
+  </Router>
+)
+
+
 
 export default App;
---
+++ my-app/src/components/auth/Login.js
@@ -0,0 +1,38 @@
+import React, {Fragment, useState} from 'react'
+import { Link } from 'react-router-dom'
+import Login1 from '../../img/Group 7.svg'
+import Google from '../../img/google.svg'
+import Facebook from '../../img/facebook.svg'
+import { Input, Button } from 'antd';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+
+const Login = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
+
+    const {  email, password } = formData;
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value});
+    const onSubmit = async e => {
+        e.preventDefault();
+            console.log('Success')
+        }
+    
+    return (
+        <Fragment>
+            <img className="Group7" src={Login1} alt=""/>
+            <h1 className="large text-primary test">Welcome To Muzo</h1>
+            <h1 className="Continue">Continue with</h1>
+            <Input className="Group24" type="email" placeholder="       Enter Email"/>
+            <Input className="Group23" type="password"placeholder="       Enter Password"/>
+            <Button className="SignUp">
+                    Sign up
+            </Button>
+            <h1 className="OrUsing">Or using</h1>
+                <Button  className="Google"><img src={Google}/></Button>
+                <Button className="Facebook"><img src={Facebook}/></Button>
+        </Fragment>
+    )
+}
+export default Login;
