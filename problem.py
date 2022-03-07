b754a50e91605e6ca8687b75fc69d2b334b0b28f
Yogesh7988 <101108781+Yogesh7988@users.noreply.github.com>
2022-03-07 19:41:04 +0530

    Update problem.py

--- problem.py
+++ problem.py
@@ -1,7 +1,9 @@
 import fitz
+import pandas as pd 
 file=fitz.open('pdf1.pdf')
 for page in file:
-    text=page.get_text('text')
-    txt=open(f'pdf1.json','a')
-    txt.writelines(text)
-    txt.close()
+    word=page.get_text('words')
+    #saving raw data in json file
+    wd=open(f'pdf1.json','a')
+    wd.writelines(str(word))
+    wd.close()
