c7379579f249805708e21bdab35bfafc7aefc4d7
Yogesh7988 <101108781+Yogesh7988@users.noreply.github.com>
2022-03-08 01:53:04 +0530

    Update problem.py

--- problem.py
+++ problem.py
@@ -1,9 +1,11 @@
-import fitz
-file=fitz.open('pdf1.pdf')
-for page in file:
-    text=page.get_text('text')
-    txt=open(f'pdf1.json','a')
-    txt.writelines(text)
-    txt.close()
+import sys, fitz
+fname = sys.argv[0]  
+doc = fitz.open('pdf2.pdf')  
+out = open(fname + ".txt", "wb") 
+for page in doc:  
+    text = page.get_text().encode("utf8")  
+    out.write(text)  
+    out.write(bytes((12,))) 
+out.close()
 
-    #i never worked on Pymupdf
+#i never used pymupdf lib but i'am trying my best
